88f7672dafab8446b3e98ce246ec078a068e0901
alexeykostenko <aleksey.kostenko.dev@gmail.com>
2018-02-25 16:34:18 +0200

    added main page

---
+++ resources/views/index/portfolio.blade.php
@@ -0,0 +1,268 @@
+<!-- Portfolio Grid -->
+<section class="bg-light" id="portfolio">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-12 text-center">
+        <h2 class="section-heading text-uppercase">Screenshots</h2>
+        <h3 class="section-subheading text-muted">Examples of the visual part of the admin panel.</h3>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-4 col-sm-6 portfolio-item">
+        <a class="portfolio-link" data-toggle="modal" href="#portfolioModal1">
+          <div class="portfolio-hover">
+            <div class="portfolio-hover-content">
+              <i class="fa fa-plus fa-3x"></i>
+            </div>
+          </div>
+          <img class="img-fluid" src="img/screenshots/crud.jpg" alt="">
+        </a>
+        <div class="portfolio-caption">
+          <h4>CRUD</h4>
+          <p class="text-muted">Creating CRUD</p>
+        </div>
+      </div>
+      <div class="col-md-4 col-sm-6 portfolio-item">
+        <a class="portfolio-link" data-toggle="modal" href="#portfolioModal2">
+          <div class="portfolio-hover">
+            <div class="portfolio-hover-content">
+              <i class="fa fa-plus fa-3x"></i>
+            </div>
+          </div>
+          <img class="img-fluid" src="img/screenshots/edit-role.jpg" alt="">
+        </a>
+        <div class="portfolio-caption">
+          <h4>Role</h4>
+          <p class="text-muted">Role Management</p>
+        </div>
+      </div>
+      <div class="col-md-4 col-sm-6 portfolio-item">
+        <a class="portfolio-link" data-toggle="modal" href="#portfolioModal3">
+          <div class="portfolio-hover">
+            <div class="portfolio-hover-content">
+              <i class="fa fa-plus fa-3x"></i>
+            </div>
+          </div>
+          <img class="img-fluid" src="img/screenshots/media.jpg" alt="">
+        </a>
+        <div class="portfolio-caption">
+          <h4>Media</h4>
+          <p class="text-muted">File manager</p>
+        </div>
+      </div>
+      <div class="col-md-4 col-sm-6 portfolio-item">
+        <a class="portfolio-link" data-toggle="modal" href="#portfolioModal4">
+          <div class="portfolio-hover">
+            <div class="portfolio-hover-content">
+              <i class="fa fa-plus fa-3x"></i>
+            </div>
+          </div>
+          <img class="img-fluid" src="img/screenshots/menu.jpg" alt="">
+        </a>
+        <div class="portfolio-caption">
+          <h4>Menu</h4>
+          <p class="text-muted">Menu Builder</p>
+        </div>
+      </div>
+      <div class="col-md-4 col-sm-6 portfolio-item">
+        <a class="portfolio-link" data-toggle="modal" href="#portfolioModal5">
+          <div class="portfolio-hover">
+            <div class="portfolio-hover-content">
+              <i class="fa fa-plus fa-3x"></i>
+            </div>
+          </div>
+          <img class="img-fluid" src="img/screenshots/posts.jpg" alt="">
+        </a>
+        <div class="portfolio-caption">
+          <h4>Posts</h4>
+          <p class="text-muted">Posts edit</p>
+        </div>
+      </div>
+      <div class="col-md-4 col-sm-6 portfolio-item">
+        <a class="portfolio-link" data-toggle="modal" href="#portfolioModal6">
+          <div class="portfolio-hover">
+            <div class="portfolio-hover-content">
+              <i class="fa fa-plus fa-3x"></i>
+            </div>
+          </div>
+          <img class="img-fluid" src="img/screenshots/roles.jpg" alt="">
+        </a>
+        <div class="portfolio-caption">
+          <h4>Roles</h4>
+          <p class="text-muted">Creating Roles</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+<!-- Portfolio Modals -->
+
+<!-- Modal 1 -->
+<div class="portfolio-modal modal fade" id="portfolioModal1" tabindex="-1" role="dialog" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="close-modal" data-dismiss="modal">
+        <div class="lr">
+          <div class="rl"></div>
+        </div>
+      </div>
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-8 mx-auto">
+            <div class="modal-body">
+              <!-- Project Details Go Here -->
+              <h2 class="text-uppercase">CRUD</h2>
+              <p class="item-intro text-muted">Creating CRUD</p>
+              <a target="_blank" href="http://demo.laraveladminpanel.com/admin/database">
+                <img class="img-fluid d-block mx-auto" src="img/screenshots/crud.png" alt="">
+              </a>
+              <!--p>Use this area to describe your project. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est blanditiis dolorem culpa incidunt minus dignissimos deserunt repellat aperiam quasi sunt officia expedita beatae cupiditate, maiores repudiandae, nostrum, reiciendis facere nemo!</p>
+              <ul class="list-inline">
+                <li>Date: January 2017</li>
+                <li>Client: Threads</li>
+                <li>Category: Illustration</li>
+              </ul>
+              <button class="btn btn-primary" data-dismiss="modal" type="button">
+                <i class="fa fa-times"></i>
+                Close
+              </button-->
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<!-- Modal 2 -->
+<div class="portfolio-modal modal fade" id="portfolioModal2" tabindex="-1" role="dialog" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="close-modal" data-dismiss="modal">
+        <div class="lr">
+          <div class="rl"></div>
+        </div>
+      </div>
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-8 mx-auto">
+            <div class="modal-body">
+              <h2 class="text-uppercase">Role</h2>
+              <p class="item-intro text-muted">Role Management</p>
+              <a target="_blank" href="http://demo.laraveladminpanel.com/admin/roles/1/edit">
+                <img class="img-fluid d-block mx-auto" src="img/screenshots/edit-role.png" alt="">
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<!-- Modal 3 -->
+<div class="portfolio-modal modal fade" id="portfolioModal3" tabindex="-1" role="dialog" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="close-modal" data-dismiss="modal">
+        <div class="lr">
+          <div class="rl"></div>
+        </div>
+      </div>
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-8 mx-auto">
+            <div class="modal-body">
+              <h2 class="text-uppercase">Media</h2>
+              <p class="item-intro text-muted">File manager</p>
+              <a target="_blank" href="http://demo.laraveladminpanel.com/admin/media">
+                <img class="img-fluid d-block mx-auto" src="img/screenshots/media.png" alt="">
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<!-- Modal 4 -->
+<div class="portfolio-modal modal fade" id="portfolioModal4" tabindex="-1" role="dialog" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="close-modal" data-dismiss="modal">
+        <div class="lr">
+          <div class="rl"></div>
+        </div>
+      </div>
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-8 mx-auto">
+            <div class="modal-body">
+              <!-- Project Details Go Here -->
+              <h2 class="text-uppercase">Menu</h2>
+              <p class="item-intro text-muted">Menu Builder</p>
+              <img class="img-fluid d-block mx-auto" src="img/screenshots/menu.png" alt="">
+              <a target="_blank" href="http://demo.laraveladminpanel.com/admin/menus/1/builder">
+                <img class="img-fluid d-block mx-auto" src="img/screenshots/media.png" alt="">
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<!-- Modal 5 -->
+<div class="portfolio-modal modal fade" id="portfolioModal5" tabindex="-1" role="dialog" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="close-modal" data-dismiss="modal">
+        <div class="lr">
+          <div class="rl"></div>
+        </div>
+      </div>
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-8 mx-auto">
+            <div class="modal-body">
+              <h2 class="text-uppercase">Posts</h2>
+              <p class="item-intro text-muted">Posts edit</p>
+              <a target="_blank" href="http://demo.laraveladminpanel.com/admin/posts">
+                <img class="img-fluid d-block mx-auto" src="img/screenshots/posts.png" alt="">
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<!-- Modal 6 -->
+<div class="portfolio-modal modal fade" id="portfolioModal6" tabindex="-1" role="dialog" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="close-modal" data-dismiss="modal">
+        <div class="lr">
+          <div class="rl"></div>
+        </div>
+      </div>
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-8 mx-auto">
+            <div class="modal-body">
+              <h2 class="text-uppercase">Roles</h2>
+              <p class="item-intro text-muted">Creating Roles</p>
+              <a target="_blank" href="http://demo.laraveladminpanel.com/admin/roles">
+                <img class="img-fluid d-block mx-auto" src="img/screenshots/roles.png" alt="">
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,17 @@ let mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+/*mix.js('resources/assets/js/app.js', 'public/js')
+   .sass('resources/assets/sass/app.scss', 'public/css');*/
+
+mix.sass('node_modules/bootstrap/scss/bootstrap.scss', 'public/vendor/css')
+	.copy('node_modules/font-awesome/fonts', 'public/vendor/fonts')
+	.sass('node_modules/font-awesome/scss/font-awesome.scss', 'public/vendor/css')
+    .sass('resources/assets/sass/app.scss', 'public/css')
+    .options({
+      processCssUrls: false
+    })
+    .js('resources/assets/js/app.js', 'public/js')
+    .js('resources/assets/js/jqBootstrapValidation.js', 'public/vendor/js')
+    .js('resources/assets/js/contact_me.js', 'public/js');
+mix.version();
