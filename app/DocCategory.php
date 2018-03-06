a62632561b84466d932e494d1d67e5bd7db2c3e3
alexeykostenko <aleksey.kostenko.dev@gmail.com>
2018-03-07 00:05:26 +0200

    added relations

--- app/DocCategory.php
+++ app/DocCategory.php
@@ -13,4 +13,24 @@ class DocCategory extends Model
     {
         return $this->hasMany(DocPage::class);
     }
+
+    public function parent()
+    {
+        return $this->belongsTo(DocCategory::class, 'parent_id');
+    }
+
+    public function parentId()
+    {
+        return $this->parent();
+    }
+
+    public function version()
+    {
+        return $this->belongsTo(DocVersion::class);
+    }
+
+    public function docVersionId()
+    {
+        return $this->version();
+    }
 }
--- app/DocPage.php
+++ app/DocPage.php
@@ -6,5 +6,38 @@ use Illuminate\Database\Eloquent\Model;
 
 class DocPage extends Model
 {
-    //
+    public function category()
+    {
+        return $this->belongsTo(DocCategory::class);
+    }
+
+    public function docCategoryId()
+    {
+        return $this->category();
+    }
+
+    public function parent()
+    {
+        return $this->belongsTo(DocPage::class, 'parent_id');
+    }
+
+    public function parentId()
+    {
+        return $this->parent();
+    }
+
+    public function nextDocPageId()
+    {
+        return $this->parent();
+    }
+
+    public function version()
+    {
+        return $this->belongsTo(DocVersion::class);
+    }
+
+    public function docVersionId()
+    {
+        return $this->version();
+    }
 }
